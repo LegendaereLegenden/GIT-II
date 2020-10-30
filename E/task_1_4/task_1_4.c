84cc710e95d9e9ef271bab0db6bad912029e0637
TimeScramble <71872317+TimeScramble@users.noreply.github.com>
2020-10-30 15:23:26 +0100

    Create task_1_4.c

---
+++ E/task_1_4/task_1_4.c
@@ -0,0 +1,19 @@
+/*
+ * task_1_4.c
+ *
+ *  Created on: 30.10.2020
+ *      Author: lars
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	long i = 0;
+	i = EOF;
+
+	printf("%ld", i); 	// printing the value of 'EOF'
+
+	return 0;
+}
